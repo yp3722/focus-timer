33fa824edd8023202fdbd7d53982d103e0ebade2
yash <yp3722@gmail.com>
2024-07-13 00:25:57 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="main-container">
+          <div className="left-child">
+            <p>Test-left</p>
+          </div>
+          <div className="right-child">
+            <p>Test-right </p>
+            <h4>Basic pomodoro timer :</h4>
+            <ul>
+            <li>Setup npm - done</li>
+            <li>Setup react - done</li>
+            <li>Create homepage layout - done</li>
+            <li>Design ui</li>
+            <li>Code components</li>
+            <li>assemble</li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
